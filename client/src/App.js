5a593a228d7069174a31ca1cc080a1673828736f
lyndsiWilliams <kcatgirl@gmail.com>
2020-01-31 10:47:46 -0600

    Set up authentication and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+// React
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+// Components
+import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
+// Styling
 import "./styles.scss";
+
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <PrivateRoute exact path='/protected' component={BubblePage} />
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Route path='/login' component={Login} />
       </div>
     </Router>
   );
